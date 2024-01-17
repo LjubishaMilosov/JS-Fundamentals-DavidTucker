388d944afce565ff5020c1ff2829303af32bfb3b
Ljubisha Milosov <ljubisa.milosov@gmail.com>
2024-01-18 03:53:17 +0800

    loop control flow

---
+++ 06/demos/fundamentals-javascript-m6-exercise-files/example/LMloop-flow.js
@@ -0,0 +1,33 @@
+//Array of complex objects
+import employees from './data.json' assert {type:'json'}
+
+//Listing only the first five items
+
+for(let i = 0; i < employees.length; i++){
+    if(i === 5){
+        break;
+    }
+    console.log(`Name: ${employees[i].firstName} ${employees[0].lastName}`);
+}
+
+console.log('----------------');
+
+
+// Seaching only for first names tha start with "B"
+for(let employee of employees){
+    if(!employee.firstName.startsWith("B")){
+        continue;
+    }
+    console.log(`Name: ${employee.firstName} ${employee.lastName}`);
+}
+
+//Nested loop control flow
+employee: for(let emp of employees){
+    for(let property in emp){
+        if(property === 'dateBirth'){
+            continue employee;
+        }
+        console.log(`${property}: ${emp[property]}`);
+    }
+    console.log('--------');
+}
